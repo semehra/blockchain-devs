559a60403f68639b3b57012b4561d0f316fb0b3d
Sejal Mehra <sejal.mehra@moodys.com>
2023-02-22 10:46:43 -0500

    lambda TF skeleton

--- main.tf
+++ main.tf
@@ -13,11 +13,34 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+resource "aws_lambda_function" "lambda_docs_function" {
+  filename      = "README.md"
+  function_name = "lambda-function"
+  role          = aws_iam_role.lambda_exec_iam_role.arn
+  handler       = "get_export.lambda_handler"
+  runtime       = "python3.8"
 
-  tags = {
-    Name = "ExampleAppServerInstance"
+  environment {
+    variables = {
+      restApiId = "Rest api name"
+      stageName = "api"
+    }
   }
+}
+
+resource "aws_iam_role" "lambda_exec_iam_role" {
+  name               = "lambda_function"
+  assume_role_policy = file("./api/iam/lambda_role.json")
+}
+
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+  name        = "lambda_function"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy      = file("./api/iam/lambda_policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+  role       = aws_iam_role.lambda_exec_iam_role.name
+  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
 }
